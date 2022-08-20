6756f5895a06240be5c058e2d48278dae75cd1be
begumlg <olgunbegum@gmail.com>
2022-08-19 17:47:42 -0700

    initial commit

--- WMAD-SWIFT/main.swift
+++ WMAD-SWIFT/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+let question1 = Question1()
+question1.solution1()
 
+let question2 = Question2()
+question2.solution2()
+
+let question3 = Question3()
+question3.solution3()
+
+let question4 = Question4()
+question4.solution4()
+
+let question5 = Question5()
+question5.solution5()
